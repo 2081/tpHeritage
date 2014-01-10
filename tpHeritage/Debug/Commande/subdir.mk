c5bdac1f35ec7181fba1024f48a980f338f7962c
jeje42 <jerome.guidon@insa-lyon.fr>
2014-01-10 17:30:30 +0100

    Mise en place de l'arborescence dossier

---
+++ tpHeritage/Debug/Commande/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Commande/CmdAjouterElement.cpp \
+../Commande/CmdDeplacer.cpp \
+../Commande/CmdGrouper.cpp \
+../Commande/CmdSupprimer.cpp \
+../Commande/CmdVider.cpp \
+../Commande/Commande.cpp \
+../Commande/Controleur.cpp 
+
+OBJS += \
+./Commande/CmdAjouterElement.o \
+./Commande/CmdDeplacer.o \
+./Commande/CmdGrouper.o \
+./Commande/CmdSupprimer.o \
+./Commande/CmdVider.o \
+./Commande/Commande.o \
+./Commande/Controleur.o 
+
+CPP_DEPS += \
+./Commande/CmdAjouterElement.d \
+./Commande/CmdDeplacer.d \
+./Commande/CmdGrouper.d \
+./Commande/CmdSupprimer.d \
+./Commande/CmdVider.d \
+./Commande/Commande.d \
+./Commande/Controleur.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Commande/%.o: ../Commande/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ tpHeritage/tpHeritage.cpp
@@ -0,0 +1,13 @@
+/*
+ * tpHeritage.cpp
+ *
+ *  Created on: 10 janv. 2014
+ *      Author: jeje
+ */
+#include "Commande.h"
+
+
+int main()
+{
+	Commande commande;
+}
