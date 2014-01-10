166c3f496610282482e3c96215ca32a35bb7acfc
jeje42 <jerome.guidon@insa-lyon.fr>
2014-01-10 16:52:43 +0100

    Création de la classe Controleur

---
+++ tpHeritage/Controleur.h
@@ -0,0 +1,76 @@
+/*************************************************************************
+                           Controleur  -  description
+                             -------------------
+    début                : 10 janv. 2014
+    copyright            : (C) 2014 par jeje
+*************************************************************************/
+
+//---------- Interface de la classe <Controleur> (fichier Controleur.h) ------
+#if ! defined ( CONTROLEUR_H_ )
+#define CONTROLEUR_H_
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+
+//------------------------------------------------------------------------ 
+// Rôle de la classe <Controleur>
+//
+//
+//------------------------------------------------------------------------ 
+
+class Controleur
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Controleur & operator = ( const Controleur & unControleur );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Controleur ( const Controleur & unControleur );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Controleur ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Controleur ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE 
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//--------------------------- Autres définitions dépendantes de <Controleur>
+
+#endif // CONTROLEUR_H_
+
