6f20f8fb098767694148e099751ea657ddde0b41
jeje42 <jerome.guidon@insa-lyon.fr>
2014-01-17 16:19:09 +0100

    Création de CmdSequence

---
+++ tpHeritage/Commande/CmdSequence.h
@@ -0,0 +1,78 @@
+/*************************************************************************
+                           CmdSequence  -  description
+                             -------------------
+    début                : 17 janv. 2014
+    copyright            : (C) 2014 par jeje
+*************************************************************************/
+
+//---------- Interface de la classe <CmdSequence> (fichier CmdSequence.h) ------
+#if ! defined ( CMDSEQUENCE_H_ )
+#define CMDSEQUENCE_H_
+
+//--------------------------------------------------- Interfaces utilisées
+#include "Commande.h"
+
+
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+
+//------------------------------------------------------------------------ 
+// Rôle de la classe <CmdSequence>
+//
+//
+//------------------------------------------------------------------------ 
+
+class CmdSequence : public Commande
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    CmdSequence & operator = ( const CmdSequence & unCmdSequence );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    CmdSequence ( const CmdSequence & unCmdSequence );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    CmdSequence ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~CmdSequence ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE 
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//--------------------------- Autres définitions dépendantes de <CmdSequence>
+
+#endif // CMDSEQUENCE_H_
+
