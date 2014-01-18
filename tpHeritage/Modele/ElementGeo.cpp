b0cbfe727fd516feff18eb87eed6a892029e9a37
jeje42 <jerome.guidon@insa-lyon.fr>
2014-01-18 14:18:47 +0100

    Update ElementGeo.cpp

--- tpHeritage/Modele/ElementGeo.cpp
+++ tpHeritage/Modele/ElementGeo.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 //------------------------------------------------------ Include personnel
 #include "ElementGeo.h"
+#include "Groupe.h"
 
 //------------------------------------------------------------- Constantes
 
@@ -27,14 +28,6 @@ using namespace std;
 //{
 //} //----- Fin de Méthode
 
-bool ElementGeo::Deplacer(int dx, int dy, int id = 0)
-{
-	if(id == 0 || id != dernier_deplacement){
-		dernier_deplacement = id;
-		return true;
-	}
-	return false;
-}
 
 //------------------------------------------------- Surcharge d'opérateurs
 /*ElementGeo & ElementGeo::operator = ( const ElementGeo & unElementGeo )
@@ -55,16 +48,7 @@ ElementGeo::ElementGeo ( const ElementGeo & unElementGeo )
 } //----- Fin de ElementGeo (constructeur de copie)
 
 
-ElementGeo::ElementGeo ( string fnom) : nom(fnom)
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au constructeur de <ElementGeo>" << endl;
-#endif
-    this->dependance = 0;
-    this->dernier_deplacement = 0;
-} //----- Fin de ElementGeo
+
 
 
 ElementGeo::~ElementGeo ( )
